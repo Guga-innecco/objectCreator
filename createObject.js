3d4be16a19922aae211518f2bc307c9aef8f923a
Guga-innecco <87543901+Guga-innecco@users.noreply.github.com>
2021-09-25 15:41:40 -0300

    Update createObject.js

--- createObject.js
+++ createObject.js
@@ -6,6 +6,6 @@ function set_JSON_Object(object) {
     JSON.constructor(object)
 }
 
-function detector_scripts_JSON(object) {
+function detector_scripts_JSON() {
     JSON.constructor.prototype.Scopes
 }
